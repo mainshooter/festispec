0f0ecb1d8a8e8f30b746092b34c392963639e32c
Mike Pas <mike.pas.98@gmail.com>
2019-11-05 21:26:03 +0100

    Renamed KVK to COC

--- Festispec/Festispec.Domain/Customer.cs
+++ Festispec/Festispec.Domain/Customer.cs
@@ -25,7 +25,6 @@ namespace Festispec.Domain
     
         public int Id { get; set; }
         public string Name { get; set; }
-        public int KVK { get; set; }
         public int BranchNumber { get; set; }
         public string Street { get; set; }
         public int HouseNumber { get; set; }
@@ -35,6 +34,7 @@ namespace Festispec.Domain
         public string Email { get; set; }
         public string Website { get; set; }
         public string Logo { get; set; }
+        public int COC { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ContactPerson> ContactPersons { get; set; }
