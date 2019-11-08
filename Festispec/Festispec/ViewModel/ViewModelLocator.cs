03425d29f6658437f3ada83bb285d5a32b298d2b
Peter Romijn <peter@samebestdevelopment.nl>
2019-11-08 17:28:54 +0100

    First example of a graph

--- Festispec/Festispec/ViewModel/ViewModelLocator.cs
+++ Festispec/Festispec/ViewModel/ViewModelLocator.cs
@@ -2,6 +2,8 @@ using CommonServiceLocator;
 using Festispec.ViewModel.report.element;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace Festispec.ViewModel
 {
@@ -26,7 +28,17 @@ namespace Festispec.ViewModel
         public ChartElementVM ChartElement 
         {
             get {
-                return ServiceLocator.Current.GetInstance<ChartElementVM>();
+                var chartElement = ServiceLocator.Current.GetInstance<ChartElementVM>();
+                chartElement.Title = "Test titel";
+                chartElement.Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+                chartElement.XaxisName = "Test xas";
+                chartElement.YaxisName = "Test y";
+                chartElement.SeriesCollection = new SeriesCollection
+                {
+                    new LineSeries { Title = "Bezoekers", Values = new ChartValues<int> {40, 60, 50, 20, 40, 60}}
+                };
+                chartElement.SeriesCollection.Add(new LineSeries { Title = "Test 2", Values = new ChartValues<int> { 44, 587, 10, 5, 9, 60 } });
+                return chartElement;
             }
         }
         
