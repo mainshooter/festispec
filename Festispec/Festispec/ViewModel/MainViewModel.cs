1f6231fafe8a49077447c30d5ddf744023e8bf09
Jasper van Hienen <ja.vanhienen@student.avans.nl>
2019-11-12 13:03:04 +0100

    Due to the app.csproj being corruped, files have been re-added

--- Festispec/Festispec/ViewModel/MainViewModel.cs
+++ Festispec/Festispec/ViewModel/MainViewModel.cs
@@ -1,16 +1,81 @@
-using System.Windows;
-using System.Windows.Media.Animation;
-using Festispec.View;
+using Festispec.Singleton;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Festispec.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        //privates
+        private Page _page;
+        private PageSingleton _pageSingleton;
+
+        //publics
+        public ICommand CloseApplication { get; set; }
+        public ICommand OpenDashboard { get; set; }
+        public ICommand OpenEmployee { get; set; }
+        public ICommand OpenCustomer { get; set; }
+        public ICommand OpenAvailability { get; set; }
+        public ICommand OpenEvent { get; set; }
+        public ICommand OpenSick { get; set; }
+
+        public Page Page
+        {
+            get { return _page; }
+            set { _page = value; RaisePropertyChanged("Page"); }
+        }
+
+        //constructor
         public MainViewModel()
         {
-            var login = new LoginWindow();
-            login.Show();
+            CloseApplication = new RelayCommand(CloseApp);
+            OpenDashboard = new RelayCommand(OpenDashboardTab);
+            OpenEmployee = new RelayCommand(OpenEmployeeTab);
+            OpenCustomer = new RelayCommand(OpenCustomerTab);
+            OpenAvailability = new RelayCommand(OpenAvailabilityTab);
+            OpenEvent = new RelayCommand(OpenEventTab);
+            OpenSick = new RelayCommand(OpenSickTab);
+            _pageSingleton = new PageSingleton();
+
+            Page = _pageSingleton.GetPage("dashboard");
+        }
+
+        //methodes
+        private void CloseApp()
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void OpenDashboardTab()
+        {
+            Page = _pageSingleton.GetPage("dashboard");
+        }
+
+        private void OpenEmployeeTab()
+        {
+            Page = _pageSingleton.GetPage("employee");
+        }
+
+        private void OpenCustomerTab()
+        {
+            Page = _pageSingleton.GetPage("customer");
+        }
+
+        private void OpenAvailabilityTab()
+        {
+            Page = _pageSingleton.GetPage("availability");
+        }
+
+        private void OpenEventTab()
+        {
+            Page = _pageSingleton.GetPage("event");
+        }
+
+        private void OpenSickTab()
+        {
+            Page = _pageSingleton.GetPage("sick");
         }
     }
 }
