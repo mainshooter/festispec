550b413ac794be31488f9eefedc3e5b4deb327e4
Jasper van Hienen <ja.vanhienen@student.avans.nl>
2019-11-13 14:07:37 +0100

    Re-added ASP project, including tests

---
+++ Festispec/Festispec.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Festispec.WebApp.Startup))]
+namespace Festispec.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
