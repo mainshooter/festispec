202634c11e51fd4304168636d0e19364491db0e1
Jasper van Hienen <ja.vanhienen@student.avans.nl>
2019-11-13 17:56:55 +0100

    Added library project for shared functionality

---
+++ Festispec/Festispec.Lib/Auth/PasswordService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Festispec.Lib.Auth
+{
+    class PasswordService
+    {
+        public string StringToPassword(string text)
+        {
+            var data = Encoding.ASCII.GetBytes(text);
+            data = new SHA256Managed().ComputeHash(data);
+            return Encoding.ASCII.GetString(data);
+        }
+
+        public bool PasswordsCompare(string stringUnHashed, string hashed)
+        {
+            return hashed == StringToPassword(stringUnHashed);
+        }
+    }
+}
---
+++ Festispec/Festispec.Web/Controllers/Auth/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Festispec.Domain;
+
+namespace Festispec.Web.Controllers.Auth
+{
+    [RoutePrefix("login")]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            Employee result = null;
+
+            using (var ctx = new Entities())
+            {
+                var employee = ctx.Employees.FirstOrDefault(i => i.Email == email);
+            }
+
+            return View(result);
+        }
+    }
+}
