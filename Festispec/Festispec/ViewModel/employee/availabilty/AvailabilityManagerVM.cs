14a1184914e363833a669d983820dc4d40b599f1
dsilvaba <49752895+dsilvaba@users.noreply.github.com>
2019-11-18 22:01:13 +0100

    Availability Week, Management

---
+++ Festispec/Festispec/ViewModel/employee/availabilty/AvailabilityManagerVM.cs
@@ -0,0 +1,94 @@
+﻿using Festispec.Domain;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Festispec.ViewModel.employee.availabilty
+{
+    public class AvailabilityManagerVM
+    {
+        public ObservableCollection<AvailabilityWeekVM> Weeks { get; set; }
+        private AvailabilityWeekVM _selectedWeek;
+        public AvailabilityWeekVM SelectedWeek
+        {
+            get
+            {
+                return _selectedWeek;
+            }
+            set
+            {
+                _selectedWeek = value;
+            }
+        }
+        public ICommand NextWeekCommand { get; set; }
+        public ICommand PreviousWeekCommand { get; set; }
+
+        public AvailabilityManagerVM()
+        {
+            SelectedWeek = new AvailabilityWeekVM(CurrentWeek());
+            Weeks = new ObservableCollection<AvailabilityWeekVM>();
+            Weeks.Add(SelectedWeek);
+            NextWeekCommand = new RelayCommand(NextWeek);
+            PreviousWeekCommand = new RelayCommand(PreviousWeek, CanSwitchWeek);
+        }
+
+        public void NextWeek()
+        {
+            if (SelectedWeek.NextWeek == null)
+            {
+                SelectedWeek.NextWeek = new AvailabilityWeekVM(SelectedWeek.Week.AddDays(7));
+                Weeks.Add(SelectedWeek.NextWeek);
+            }
+            SelectedWeek = SelectedWeek.NextWeek;
+        }
+
+        public void PreviousWeek()
+        {
+            SelectedWeek = SelectedWeek.PreviousWeek;
+        }
+
+        public bool CanSwitchWeek()
+        {
+            if (SelectedWeek.PreviousWeek == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void SaveChanges()
+        {
+            using (var context = new Entities())
+            {
+                foreach (var week in Weeks)
+                {
+                    context.Entry(week.Monday).State = EntityState.Modified;
+                    context.Entry(week.Tuesday).State = EntityState.Modified;
+                    context.Entry(week.Wednesday).State = EntityState.Modified;
+                    context.Entry(week.Thursday).State = EntityState.Modified;
+                    context.Entry(week.Friday).State = EntityState.Modified;
+                    context.Entry(week.Saturday).State = EntityState.Modified;
+                    context.Entry(week.Sunday).State = EntityState.Modified;
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public bool CanSaveChanges()
+        {
+            return true;
+        }
+
+        public DateTime CurrentWeek()
+        {
+            int diff = (7 + (DateTime.Today.DayOfWeek - DayOfWeek.Monday)) % 7;
+            return DateTime.Today.AddDays(-1 * diff).Date;
+        }
+    }
+}
---
+++ Festispec/Festispec/ViewModel/employee/availabilty/AvailabilityWeekVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festispec.ViewModel.employee.availabilty
+{
+    public class AvailabilityWeekVM
+    {
+        public DateTime Week { get; set; }
+        public AvailabilityWeekVM NextWeek { get; set; }
+        public AvailabilityWeekVM PreviousWeek { get; set; }
+        public AvailabiltyVM Monday { get; set; }
+        public AvailabiltyVM Tuesday { get; set; }
+        public AvailabiltyVM Wednesday { get; set; }
+        public AvailabiltyVM Thursday { get; set; }
+        public AvailabiltyVM Friday { get; set; }
+        public AvailabiltyVM Saturday { get; set; }
+        public AvailabiltyVM Sunday { get; set; }
+
+        public AvailabilityWeekVM(DateTime week)
+        {
+            Week = week;
+            Monday = new AvailabiltyVM();
+            Tuesday = new AvailabiltyVM();
+            Wednesday = new AvailabiltyVM();
+            Thursday = new AvailabiltyVM();
+            Friday = new AvailabiltyVM();
+            Saturday = new AvailabiltyVM();
+            Sunday = new AvailabiltyVM();
+        }
+
+    }
+}
