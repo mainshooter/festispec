5cb9fd1ffed3fe6be63388469809120e90810918
Jasper van Hienen <ja.vanhienen@student.avans.nl>
2019-11-18 14:12:52 +0100

    Added pages

---
+++ Festispec/Festispec.Web/Controllers/SurveyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace Festispec.Web.Controllers
+{
+    public class SurveyController : Controller
+    {
+        // GET: Survey
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
