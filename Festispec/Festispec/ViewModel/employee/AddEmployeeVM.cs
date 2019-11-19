718dcba248abfe2a17e2a0494888b23e7c710f30
Mike Pas <mike.pas.98@gmail.com>
2019-11-19 15:45:28 +0100

    Used Daniel's changes and changed encrypt method and made iban case insensitive

--- Festispec/Festispec/ViewModel/employee/AddEmployeeVM.cs
+++ Festispec/Festispec/ViewModel/employee/AddEmployeeVM.cs
@@ -1,5 +1,4 @@
 ﻿using Festispec.Domain;
-using Festispec.Lib.Auth;
 using Festispec.Message;
 using Festispec.View.Pages.Employee;
 using Festispec.ViewModel.employee.department;
@@ -11,6 +10,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Festispec.Lib.Auth;
 
 namespace Festispec.ViewModel.employee
 {
@@ -45,66 +45,6 @@ namespace Festispec.ViewModel.employee
             Employee.Password = passwordService.StringToPassword(Employee.Password);
         }
 
-        public void AddEmployee()
-        {
-            Encrypt();
-            using (var context = new Entities())
-            {
-                context.Employees.Add(Employee.ToModel());
-                context.SaveChanges();
-            }
-            CloseAddEmployee();
-        }
-
-        public bool CanAddEmployee(PasswordBox password)
-        {
-            if (password == null)
-            {
-                return false;
-            }
-            if (Employee.Firstname == null || Employee.Lastname == null || password.Password == null || password.Password.Equals("") || Employee.City == null || Employee.Department == null || Employee.Email == null || Employee.HouseNumber <= 0 || Employee.Phone == null || Employee.PostalCode == null || Employee.Status == null || Employee.Street == null || Employee.Birthday.ToString().Equals("") || Employee.Birthday == null || Employee.Iban == null)
-            {
-                return false;
-            }
-            string regexEmail = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
-            string regexPhone = "^\\s*(?:\\+?(\\d{1,3}))?([-. (]*(\\d{3})[-. )]*)?((\\d{3})[-. ]*(\\d{2,4})(?:[-.x ]*(\\d+))?)\\s*$";
-            string regexIban = "^([A-Z]{2}[ -]?[0-9]{2})(?=(?:[ -]?[A-Z0-9]){9,30}$)((?:[ -]?[A-Z0-9]{3,5}){2,7})([ -]?[A-Z0-9]{1,3})?$";
-
-            if (Employee.Prefix != null)
-            {
-                if (Employee.Prefix.Length > 45)
-                {
-                    return false;
-                }
-            }
-
-            if (Employee.HouseNumberAddition != null)
-            {
-                if (Employee.HouseNumberAddition.Length > 5)
-                {
-                    return false;
-                }
-            }
-            if (!Regex.IsMatch(Employee.Email, regexEmail))
-            {
-                return false;
-            }
-            if (!Regex.IsMatch(Employee.Phone, regexPhone))
-            {
-                return false;
-            }
-            if (!Regex.IsMatch(Employee.Iban, regexIban))
-            {
-                return false;
-            }
-            if (Employee.Birthday.Year < 1900 || Employee.Birthday > DateTime.Today || Employee.Iban.Length > 27 || password.Password.Length > 255 || Employee.Email.Length > 100 || Employee.Phone.Length > 15 || Employee.City.Length > 45 || Employee.PostalCode.Length > 6 || Employee.HouseNumber > 9999 || Employee.Street.Length > 100 || Employee.Lastname.Length > 45 || Employee.Firstname.Length > 45)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
         public void AddEmployee(PasswordBox password)
         {
             Employee.Password = password.Password;
@@ -127,6 +67,41 @@ namespace Festispec.ViewModel.employee
             Employee = new EmployeeVM();
             RaisePropertyChanged("Employee");
             MessengerInstance.Send<ChangePageMessage>(new ChangePageMessage() { NextPageType = typeof(EmployeePage) });
+        }
+
+        public bool CanAddEmployee(PasswordBox password)
+        {
+            if (Employee.Firstname == null || Employee.Lastname == null || password.Password == null || password.Password.Equals("") || Employee.City == null || Employee.Department == null || Employee.Email == null || Employee.HouseNumber <= 0 || Employee.Phone == null || Employee.PostalCode == null || Employee.Status == null || Employee.Street == null || Employee.Birthday.ToString().Equals("") || Employee.Birthday == null || Employee.Iban == null)
+            {
+                return false;
+            }
+
+            string regexEmail = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+            string regexPhone = "^\\s*(?:\\+?(\\d{1,3}))?([-. (]*(\\d{3})[-. )]*)?((\\d{3})[-. ]*(\\d{2,4})(?:[-.x ]*(\\d+))?)\\s*$";
+            string regexIban = "^(?i)([A-Z]{2}[ -]?[0-9]{2})(?=(?:[ -]?[A-Z0-9]){9,30}$)((?:[ -]?[A-Z0-9]{3,5}){2,7})([ -]?[A-Z0-9]{1,3})?$";
+
+            if (Employee.Prefix != null)
+            {
+                if (Employee.Prefix.Length > 45)
+                {
+                    return false;
+                }
+            }
+
+            if (Employee.HouseNumberAddition != null)
+            {
+                if (Employee.HouseNumberAddition.Length > 5)
+                {
+                    return false;
+                }
+            }
+
+            if (!Regex.IsMatch(Employee.Email, regexEmail) || !Regex.IsMatch(Employee.Phone, regexPhone) || !Regex.IsMatch(Employee.Iban, regexIban) || Employee.Birthday.Year < 1900 || Employee.Birthday > DateTime.Today || Employee.Iban.Length > 27 || password.Password.Length > 255 || Employee.Email.Length > 100 || Employee.Phone.Length > 15 || Employee.City.Length > 45 || Employee.PostalCode.Length > 6 || Employee.HouseNumber > 9999 || Employee.Street.Length > 100 || Employee.Lastname.Length > 45 || Employee.Firstname.Length > 45)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
