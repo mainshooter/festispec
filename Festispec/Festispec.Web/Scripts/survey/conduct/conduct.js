94a7ba75f3f825c28daa402069769c667adf119b
Peter Romijn <paromijn@avans.nl>
2019-12-04 17:54:16 +0100

    wip

--- Festispec/Festispec.Web/Scripts/survey/conduct/conduct.js
+++ Festispec/Festispec.Web/Scripts/survey/conduct/conduct.js
@@ -1,3 +1,18 @@
-﻿(function() {
+﻿class Survey {
+
+	// SCOPE - container
+    constructor(identifer) {
+        this.scope = document.querySelector(identifer);
+    }
+
+    getQuestion() {
+        let questions = [];
+
+    }
+}
+
+
+(function() {
+
 console.log("doei doei");
 })();
