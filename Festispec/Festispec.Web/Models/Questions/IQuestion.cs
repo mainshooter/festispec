7a9a363ad772445ba4c470c8163485987cefbfc4
Jasper van Hienen <ja.vanhienen@student.avans.nl>
2019-12-05 00:17:02 +0100

    Vraagherkenning/jasper (#21)
    * setup, edmx
    
    * AddEmployee
    
    * datagrid
    
    * Addemployee
    
    * Update ViewModelLocator.cs
    
    * Employeelistvm
    
    * Navigation addemployee
    
    * EditEmployee
    
    * Delete
    
    * Medewerker toevoegen button
    
    * Birthday
    
    * Added login functionality
    
    * Icons
    
    * Updated files
    
    * Due to the app.csproj being corruped, files have been re-added
    
    * Made login window a page
    
    * Update
    
    * Modified the password encryption
    
    * var toegevoegd
    
    * Re-added ASP project, including tests
    
    * Updated ignore and web config
    
    * Messed around with projects
    
    * Added library project for shared functionality
    
    * WPF implements new class from Lib project
    
    * Code cleanup
    
    * Added popup confirmation
    
    * Added login logic
    
    * Fixed comments
    
    * Refectors
    
    * Fnished login feature in ASP
    
    * Fixed other comments
    
    * Added config to the Web.Test project
    
    * Made sessions typed
    
    * Added logout functionality
    
    * Added redirect after logout
    
    * Class library support .net framework
    
    * Removed no longer needed file
    
    * Added dotsettings to ignore
    
    * Fixed comments
    
    * Config opnieuw teruggezet
    
    * Refactors
    
    * Removed iclosable
    
    * Removed using
    
    * Removed using
    
    * Ignore update
    
    * Added pages
    
    * Prepared for recognition logic
    
    * Refactors
    
    * Prepared controllers and views
    
    * Backup
    
    * Finished all question types, exluding the table question
    
    * Code cleanup
    
    * Refactor to Eric
    
    * Fixed comments
    
    * Fix more comments
    
    * Removed folder
    
    * Made maximum dynamic
    
    * Moved description up
    
    * Swag

---
+++ Festispec/Festispec.Web/Models/Questions/IQuestion.cs
@@ -0,0 +1,12 @@
+﻿using Festispec.Lib.Survey.Question;
+
+namespace Festispec.Web.Models.Questions
+{
+    public interface IQuestion
+    {
+        int Id { get; set; }
+        QuestionDetails Details { get; set; }
+        int AnswerValue { get; set; }
+        int AnswerText { get; set; }
+    }
+}
