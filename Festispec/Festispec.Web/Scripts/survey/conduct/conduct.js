785f58a9d1901018ffa060b7d436fc517a915e87
Peter Romijn <peter@samebestdevelopment.nl>
2019-12-07 13:23:30 +0100

    Offline storage working with case upload

--- Festispec/Festispec.Web/Scripts/survey/conduct/conduct.js
+++ Festispec/Festispec.Web/Scripts/survey/conduct/conduct.js
@@ -1,124 +1,120 @@
-﻿(function () {
+﻿if (!window.Festispec) {
+	window.Festispec = {};
+}
+Festispec.storage = window.localStorage;
 
-})();
-
-function test() {
-	var kvpairs = [];
-	var form = document.querySelector("form");
-for (var i = 0; i < form.elements.length; i++) {
-	var e = form.elements[i];
-	console.log(e.type);
-	if (e.type == "file") {
-		let reader = new FileReader();
-		let files = e.files;
-		for (var j = 0; j < files.length; j++) {
-			let file = files[j];
-			reader.readAsDataURL(file);
-			reader.onloadend = function (evt) {
-				console.log(evt.target.result);
-				console.log(reader.result);
+function saveAllFormInputs() {
+	let promises = [];
+	let form = document.querySelector("form");
+	let formElements = form.elements;
+	for (let i = 0; i < formElements.length; i++) {
+		let element = formElements[i];
+		let type = element.type;
+		if (type == "file") {
+			let files = element.files;
+			for (let j = 0; j < files.length; j++) {
+				let file = files[j];
+				promises.push(blobToBase64(element.name, file));
 			}
-			reader.onerror = function (error) { console.log(error); }
 		}
-
+		else {
+			promises.push(getInputValue(element));
+		}
 	}
-		kvpairs.push(encodeURIComponent(e.name) + "=" + encodeURIComponent(e.value));
-	}
-	console.log(kvpairs);
+	Promise.all(promises).then((values) => {
+		console.log(values);
+		saveToLocalStorage(values);
+	});
 }
 
 
+async function getInputValue(inputElement) {
+	return new Promise((resolve, error) => {
+		resolve({
+			value: inputElement.value,
+			name: inputElement.name,
+		});
+	});
+}
 
-(function ($) {
-	$.fn.serializeFiles = function () {
-		var form = $(this),
-			formData = new FormData()
-		formParams = form.serializeArray();
-
-		$.each(form.find('input[type="file"]'), function (i, tag) {
-			$.each($(tag)[0].files, function (i, file) {
-				formData.append(tag.name, file);
+async function blobToBase64(inputName, blob) {
+	return new Promise((resolve, error) => {
+		let reader = new FileReader();
+		reader.readAsDataURL(blob);
+		reader.onloadend = function () {
+			resolve({
+				value: reader.result,
+				name: inputName,
 			});
-		});
-
-		$.each(formParams, function (i, val) {
-			formData.append(val.name, val.value);
-		});
-
-		return formData;
-	};
-})(jQuery);
-
-(function () {
-	let storage = window.localStorage;
-	$("form").submit((event) => {
-		event.preventDefault();
-		if ($("form").valid({
-			debug: true,
-		})) {
-			let formResult = $("form").serializeArray();
-			
-			saveToLocalStorage(formResult);
-		}
-		else {
-			alert("survey not filled in correctly");
 		}
 	});
+}
 
-	function getCasesFromLocalStorage() {
-		let storageResult = storage.getItem("survey");
+function getCasesFromLocalStorage() {
+	let storageResult = Festispec.storage.getItem("survey");
 
-		if (storageResult == null) {
-			storageResult = {};
-		}
-		else {
-			storageResult = JSON.parse(storageResult);
-		}
-
-		return storageResult;
+	if (storageResult == null) {
+		storageResult = {};
+	}
+	else {
+		storageResult = JSON.parse(storageResult);
 	}
 
-	function saveToLocalStorage(result) {
-		let surveyCases = getCasesFromLocalStorage();
+	return storageResult;
+}
 
-		if (surveyCases.hasOwnProperty(surveyId)) {
-			let cases = surveyCases[surveyId];
-			cases.push(result);
-		}
-		else {
-			surveyCases[surveyId] = [];
-			let cases = surveyCases[surveyId];
-			cases.push(result);
-		}
-		localStorage.setItem("survey", JSON.stringify(surveyCases));
+function saveToLocalStorage(result) {
+	let surveyCases = getCasesFromLocalStorage();
+
+	if (surveyCases.hasOwnProperty(surveyId)) {
+		let cases = surveyCases[surveyId];
+		cases.push(result);
 	}
-
-	setInterval(() => {
-		let allCases = getCasesFromLocalStorage();
-		for (surveyCaseKey in allCases) {
-			let cases = allCases[surveyCaseKey];
-			if (cases == null && cases.length < 0) {
-				continue;
-			}
-			for (var i = 0; i < cases.length; i++) {
-				let surveyCase = cases[i];
-				uploadSurveyCase(allCases, cases, i, surveyCase);
-			}
-		}
-		localStorage.setItem("survey", JSON.stringify(allCases));
-	}, 5000);
-
-	function uploadSurveyCase(allCases, surveyCases, index, currentCase) {
-		$.ajax({
-			type: "POST",
-			data: currentCase,
-			success: function (data) {
-				surveyCases.splice(index, 1);
-				localStorage.setItem("survey", JSON.stringify(allCases));
-			},
-			error: function (errorMsg) {
-				console.log("error");
-			}
-		});
+	else {
+		surveyCases[surveyId] = [];
+		let cases = surveyCases[surveyId];
+		cases.push(result);
 	}
-})();
+	Festispec.storage.setItem("survey", JSON.stringify(surveyCases));
+}
+
+setInterval(() => {
+	let allCases = getCasesFromLocalStorage();
+	for (surveyCaseKey in allCases) {
+		let cases = allCases[surveyCaseKey];
+		if (cases == null && cases.length < 0) {
+			continue;
+		}
+		for (var i = 0; i < cases.length; i++) {
+			let surveyCase = cases[i];
+			uploadSurveyCase(allCases, cases, i, surveyCase);
+		}
+	}
+	localStorage.setItem("survey", JSON.stringify(allCases));
+}, 5000);
+
+function uploadSurveyCase(allCases, surveyCases, index, currentCase) {
+	$.ajax({
+		type: "POST",
+		data: currentCase,
+		success: function (data) {
+			surveyCases.splice(index, 1);
+			localStorage.setItem("survey", JSON.stringify(allCases));
+		},
+		error: function (errorMsg) {
+			console.log("error");
+		}
+	});
+}
+
+$("form").submit((event) => {
+	event.preventDefault();
+	$("form").validate({
+		ignore: "input[type='file']",
+	});
+	if ($("form").valid()) {
+		saveAllFormInputs();
+		$("form").trigger("reset");
+		alert("Uw case is opgeslagen");
+	}
+});
