074c860d94edc267f0f0e5ebfad35d6861acc096
Bart Fransen <bart.fransen@student.avans.nl>
2019-12-08 21:12:24 +0100

    Update SurveyController
    Fix prev Pull Request

--- Festispec/Festispec.Web/Controllers/SurveyController.cs
+++ Festispec/Festispec.Web/Controllers/SurveyController.cs
@@ -16,7 +16,7 @@ namespace Festispec.Web.Controllers
         // GET: Survey
         public ActionResult Index()
         {
-            return View(_db.Surveys.ToList());
+            return View(_db.Surveys.ToList().Where(s => s.Status == "Definitief"));
         }
 
         public ActionResult Details(int? id)
