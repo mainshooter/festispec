119627f4d915091518730c374fda24b47b7dd197
Bart Fransen <bart.fransen@student.avans.nl>
2019-12-10 16:39:58 +0100

    Tekenvraag finished
    Tekenvraag samen met mike aangemaakt en afgemaakt

--- Festispec/Festispec.Web/Models/Questions/Types/DrawingQuestionType.cs
+++ Festispec/Festispec.Web/Models/Questions/Types/DrawingQuestionType.cs
@@ -1,13 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using System.IO;
 using Festispec.Lib.Survey.Question;
+using System;
 
 namespace Festispec.Web.Models.Questions.Types
 {
     public class DrawingQuestionType : IQuestion
     {
+        private QuestionDetails _details;
         public int Id { get; set; }
-        public QuestionDetails Details { get; set; }
+
+        public QuestionDetails Details
+        {
+            get => _details;
+            set
+            {
+                _details = value;
+                ByteArrayToImageUrl();
+            }
+        }
+
+        public string ImageUrl { get => "Content/question-" + Details.Question + ".jpeg"; }
         public int AnswerValue { get; set; }
         public int AnswerText { get; set; }
+
+        public void ByteArrayToImageUrl() => File.WriteAllBytes(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Content/question-" + Details.Question + ".jpeg"), Details.Images[0]);
     }
 }
