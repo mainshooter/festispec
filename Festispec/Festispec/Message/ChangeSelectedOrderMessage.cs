a77febf0980d1d594b8f371bca8b2a87b7612bbe
Peter Romijn <paromijn@avans.nl>
2019-12-11 11:14:18 +0100

    Removed usings

--- Festispec/Festispec/Message/ChangeSelectedOrderMessage.cs
+++ Festispec/Festispec/Message/ChangeSelectedOrderMessage.cs
@@ -1,9 +1,4 @@
 ﻿using Festispec.ViewModel.Customer.order;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Festispec.Message
 {
