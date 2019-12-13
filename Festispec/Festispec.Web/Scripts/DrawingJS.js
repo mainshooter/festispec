1c2651c8d8d2eb39c2434b6747cb380dfdffb333
Bartf6 <bartfransen66@gmail.com>
2019-12-13 21:17:33 +0100

    update tekenvraag
    update tekenvraag

---
+++ Festispec/Festispec.Web/Scripts/DrawingJS.js
@@ -0,0 +1,18 @@
+﻿export class DrawingDict
+{
+    constructor()
+    {
+        this.canvasMap = new Map;
+    }
+
+    addCanvas(canvas, ID)
+    {
+        this.canvasMap.set(ID, canvas);
+        
+    }
+
+    getCanvas(ID)
+    {
+        return this.canvasMap.get(ID);
+    }
+}
