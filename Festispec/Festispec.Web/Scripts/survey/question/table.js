f150d45cc8040a57e2309394044c44e674faf0fb
Peter Romijn <peter@samebestdevelopment.nl>
2019-12-15 17:41:38 +0100

    Wip

---
+++ Festispec/Festispec.Web/Scripts/survey/question/table.js
@@ -0,0 +1,18 @@
+﻿class TableQuestion {
+
+	constructor(tableId, inputName) {
+		this.scope = document.querySelector(tableId);
+		this.inputField = document.querySelector("input[name='" + inputName + "']");
+		this.addListners();
+	}
+
+	addListners() {
+		this.scope.addEventListener('change', (event) => {
+			let tableAnswerRows = this.scope.querySelectorAll("tbody tr");
+		});
+	}
+
+	setNewValues(value) {
+		this.inputField.value = value;
+	}
+}
