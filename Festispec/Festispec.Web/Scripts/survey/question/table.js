e7f6691e16581ff900bbb566e23b394a7d9f3b5f
Peter Romijn <peter@samebestdevelopment.nl>
2019-12-18 16:20:08 +0100

    Written support table question

--- Festispec/Festispec.Web/Scripts/survey/question/table.js
+++ Festispec/Festispec.Web/Scripts/survey/question/table.js
@@ -10,12 +10,33 @@
 
 	addListners() {
 		this.scope.addEventListener('change', (event) => {
-			console.log("doei");
 			let tableAnswerRows = this.scope.querySelectorAll("tbody tr");
 			let rowAnswers = [];
 			tableAnswerRows.forEach((row) => {
-				console.log(row);
+				let tds = row.querySelectorAll("td");
+				let rowValues = [];
+				tds.forEach((tdElement) => {
+					let inputElement = tdElement.firstElementChild;
+					if (inputElement.hasAttribute("type")) {
+						// Is it a text field
+						let value = inputElement.value;
+						rowValues.push(value);
+					}
+					else {
+						// It is a select
+						let selectedValue = inputElement.querySelector("option:checked");
+						if (selectedValue == null) {
+							rowValues.push("");
+						}
+						else {
+							rowValues.push(selectedValue.value);
+						}
+					}
+				});
+				rowAnswers.push(rowValues);
 			});
+			console.log(rowAnswers);
+			this.setNewValues(JSON.stringify(rowAnswers));
 		});
 	}
 
