12be12bc2ba67f77ee1fc1848ad6ae520d5b55bc
Peter Romijn <peter@samebestdevelopment.nl>
2019-12-19 22:26:28 +0100

    Can now edit draw usercontrol

--- Festispec/Festispec/ViewModel/report/element/Edit/EditDrawVM.cs
+++ Festispec/Festispec/ViewModel/report/element/Edit/EditDrawVM.cs
@@ -1,8 +1,18 @@
-﻿
+﻿using Festispec.Lib.Enums;
+using Festispec.Message;
 
 namespace Festispec.ViewModel.report.element.Edit
 {
-    public class EditDrawVM
+    public class EditDrawVM : BaseElementEdit
     {
+        public EditDrawVM()
+        {
+            ReportElementVM = new ReportElementVM();
+            ReportElementVM.Type = ReportElementType.Barchart;
+            MessengerInstance.Register<ChangeSelectedReportElementMessage>(this, message =>
+            {
+                ReportElementVM = message.ReportElementVM;
+            });
+        }
     }
 }
