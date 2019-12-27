97e85d63b0a69354b1072ff518a03f061a6b7537
Peter Romijn <peter@samebestdevelopment.nl>
2019-12-27 12:16:27 +0100

    Removed test images

--- Festispec/Festispec.Web/Models/Questions/Types/DrawingQuestionType.cs
+++ Festispec/Festispec.Web/Models/Questions/Types/DrawingQuestionType.cs
@@ -1,18 +1,12 @@
-﻿using System.IO;
-using Festispec.Lib.Survey.Question;
-using System;
+﻿using Festispec.Lib.Survey.Question;
 
 namespace Festispec.Web.Models.Questions.Types
 {
     public class DrawingQuestionType : IQuestion
     {
         private QuestionDetails _details;
-        
         public int Id { get; set; }
-        
         public QuestionDetails Details { get; set; }
-
-        public string ImageUrl { get => "Content/question-" + Details.Question + ".jpeg"; }
         public string DetailsJson { get; set; }
         public int AnswerValue { get; set; }
         public int AnswerText { get; set; }
